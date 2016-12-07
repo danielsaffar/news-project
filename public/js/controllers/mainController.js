4ef125a91ca455dcacfd802b4c7136c53db30fa8
Isaac BA <iba.burton@gmail.com>
2016-12-07 09:40:34 +0200

    commit

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -2,9 +2,7 @@ app.controller('MainCtrl', ['$scope', 'newsFactory', function($scope, newsFactor
 
   $scope.articles = newsFactory.articles;
 
-  $scope.newsAPI= function(){
-
-    alert('WORK');
+    $scope.newsAPI = function(){
 
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     url += '?' + $.param({
@@ -15,13 +13,39 @@ app.controller('MainCtrl', ['$scope', 'newsFactory', function($scope, newsFactor
       url: url,
       method: 'GET',
     }).done(function(data) {
-      console.log(data);
+      // console.log(data);
+      $scope.query(data);
+
     }).fail(function(err) {
       throw err;
-    })
+    })       
+
+  };
+
+  $scope.query = function(data){
+
+    for(var i = 0; i < data.response.docs.length; i++){
+
+      var article = {
+        id:data.response.docs[i]._id,
+        headline:data.response.docs[i].headline.main,
+        // image:function(data){
+        //   if(data.response.docs[i].multimedia[i].url){
+        //     article.image = data.response.docs[i].multimedia[i].url;
+        //   }else{
+        //     article.image = "https://crbechervaise.files.wordpress.com/2015/08/the_new_york_times_logo.jpg";
+        //   }
+        // },
+        link:data.response.docs[i].web_url,
+        section:data.response.docs[i].section_name,
+        subsection:data.response.docs[i].subsection_name,
+        // author:data.response.docs[i].byline.contributor,
+        snippet:data.response.docs[i].lead_paragraph
+      };
+      $scope.articles.push(article);
+    }
+    // console.log($scope.articles);
   }
-
-
 
 
 
--- public/js/services/factory.js
+++ public/js/services/factory.js
@@ -3,9 +3,25 @@ app.factory('newsFactory', ['$http', function($http){
 var news = {
   articles:[],
 
-
+  query:function(data){
+    // console.log(data);
+    for(var i = 0; i<data.response.docs[i]; i++){
+      console.log('got here');
+      var article = {
+        id:data.response.docs[i]._id,
+        headline:response.docs[i].headline.main,
+        image:response.docs[i].mulitmedia[i].url,
+        link:response.docs[i].web_url,
+        section:response.docs[i].section_name
+      }
+    this.articles.push(article);
+    console.log("pushed");
+    }   
+    for(var i =0 ; i<this.articles.length;i++){
+      console.log(this.articles[i]);
+    }
+  }
 }
-
 
 return news;
   }]);
